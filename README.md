a13e819b38b7c567d1aa778c74fe41bd2d313883
itsmesah <sahar.waryobi@gmail.com>
2023-12-11 17:26:54 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Banned-Book-Website
+ This is a Passion Project of mine that displays my love for literature and technology. The goal is to make a book hub where it simplifies the process of finding books, even banned ones, and educated readers on why books are banned. My hope is to inform those who visit the webpage about how we can combat litereature censorship!
---
+++ interactives.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Search Functionality
+    const searchInput = document.getElementById('search-input');
+    if (searchInput) {
+        searchInput.addEventListener('keyup', function(event) {
+            const searchTerm = event.target.value.toLowerCase();
+            const books = document.querySelectorAll('#books-list li');
+
+            books.forEach(function(book) {
+                const text = book.textContent.toLowerCase();
+                book.style.display = text.includes(searchTerm) ? 'block' : 'none';
+            });
+        });
+    }
+
+    // Sidebar Toggle
+    const sidebarToggle = document.getElementById('sidebar-toggle');
+    const sidebar = document.getElementById('sidebar');
+    if (sidebarToggle && sidebar) {
+        sidebarToggle.addEventListener('click', () => {
+            sidebar.classList.toggle('visible');
+        });
+    }
+
+    // Modal Pop-Up
+    const modal = document.getElementById('modal');
+    const openModalButton = document.getElementById('open-modal');
+    const closeModalButton = document.getElementById('close-modal');
+    if (openModalButton && modal && closeModalButton) {
+        openModalButton.addEventListener('click', () => {
+            modal.style.display = 'block';
+        });
+
+        closeModalButton.addEventListener('click', () => {
+            modal.style.display = 'none';
+        });
+    }
+
+    // Dark Mode Toggle
+    const darkModeToggle = document.getElementById('dark-mode-toggle');
+    if (darkModeToggle) {
+        darkModeToggle.addEventListener('click', () => {
+            document.body.classList.toggle('dark-mode');
+        });
+    }
+
+    // Image Gallery Slider - Example Functionality
+    let slideIndex = 0;
+    function showSlides() {
+        let i;
+        const slides = document.getElementsByClassName("mySlides");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}    
+        slides[slideIndex - 1].style.display = "block";  
+        setTimeout(showSlides, 4000); // Change every 4 seconds
+    }
+    showSlides();
+});
